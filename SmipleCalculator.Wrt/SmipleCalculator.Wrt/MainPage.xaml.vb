1254202a32888022982bdc6d0b0a5a9cc9de4581
SUDHANSUX <50290618+SUDHANSUX@users.noreply.github.com>
2019-07-20 18:57:12 +0530

    SmipleCalculator.Wrt

---
+++ SmipleCalculator.Wrt/SmipleCalculator.Wrt/MainPage.xaml.vb
@@ -0,0 +1,59 @@
+﻿' The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+Option Explicit On
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Dim FirstNo As Decimal
+    Dim SecondNo As Decimal
+    Dim Result As Decimal
+
+    Private Sub BtnClear_Click(sender As Object, e As RoutedEventArgs) Handles BtnClear.Click
+        TxtFirstNo.Text = ""
+        TxtSecondNo.Text = ""
+        TxtResult.Text = ""
+    End Sub
+    Private Sub BtnAdd_Click(sender As Object, e As RoutedEventArgs) Handles BtnAdd.Click
+        If Decimal.TryParse(TxtFirstNo.Text, FirstNo) And Decimal.TryParse(TxtSecondNo.Text, SecondNo) Then
+            Result = FirstNo + SecondNo
+            TxtResult.Text = Result.ToString()
+        Else
+            TxtResult.Text = "Error: Invalid Input."
+        End If
+
+    End Sub
+
+    Private Sub BtnSub_Click(sender As Object, e As RoutedEventArgs) Handles BtnSub.Click
+        If Decimal.TryParse(TxtFirstNo.Text, FirstNo) And Decimal.TryParse(TxtSecondNo.Text, SecondNo) Then
+            Result = FirstNo - SecondNo
+            TxtResult.Text = Result.ToString()
+        Else
+            TxtResult.Text = "Error: Invalid Input."
+        End If
+    End Sub
+
+    Private Sub BtnMul_Click(sender As Object, e As RoutedEventArgs) Handles BtnMul.Click
+        If Decimal.TryParse(TxtFirstNo.Text, FirstNo) And Decimal.TryParse(TxtSecondNo.Text, SecondNo) Then
+            Result = FirstNo * SecondNo
+            TxtResult.Text = Result.ToString()
+        Else
+            TxtResult.Text = "Error: Invalid Input."
+        End If
+    End Sub
+
+    Private Sub BtnDiv_Click(sender As Object, e As RoutedEventArgs) Handles BtnDiv.Click
+        If Decimal.TryParse(TxtFirstNo.Text, FirstNo) And Decimal.TryParse(TxtSecondNo.Text, SecondNo) Then
+            If SecondNo = 0 Then
+                TxtResult.Text = "Error: Divide By Zero."
+            Else
+                Result = FirstNo / SecondNo
+                TxtResult.Text = Result.ToString()
+            End If
+        Else
+            TxtResult.Text = "Error: Invalid Input."
+        End If
+    End Sub
+End Class
